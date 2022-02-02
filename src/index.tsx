4d81d322c1deb523b95db39a973bbb7d249457ce
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-02-02 09:39:06 +0100

    Setup and cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
